6fa6fcd62360b46e18b8a3eff506d2041e185912
ms-akash <msakash2003@gmail.com>
2023-04-11 15:53:38 +0530

    Comments added in division.py file

--- division/division.py
+++ division/division.py
@@ -1,4 +1,6 @@
+#function to divide two numbers
 def doDivision(dividend, divisor):
-    return dividend / divisor
+    return dividend / divisor 
 
+#printing the result of the doDivision function
 print(doDivision(10, 5))
